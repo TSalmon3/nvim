7877c475cc8b3f71f408727e9bdd3413b8609a57
tsalmon3 <43514282+TSalmon3@users.noreply.github.com>
2023-04-13 07:46:01 +0800

    sync

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,39 @@
 return {
-        "williamboman/mason.nvim",
-        build = ":MasonUpdate", -- :MasonUpdate updates registry contents
-        config = function()
-                require("mason").setup()
-        end
+
+        -- mason --
+        {
+                "williamboman/mason.nvim",
+                build = ":MasonUpdate", -- :MasonUpdate updates registry contents
+                config = function()
+                        require("mason").setup({
+                                ui = {
+                                        icons = {
+                                                package_installed = "✓",
+                                                package_pending = "➜",
+                                                package_uninstalled = "✗"
+                                        }
+                                }
+                        })
+                end
+        },
+
+        {
+                "neovim/nvim-lspconfig",
+        },
+
+        -- mason-lspconfig --
+        {
+                "williamboman/mason-lspconfig.nvim",
+                config = function()
+                        require("mason-lspconfig").setup({
+                                ensure_installed = { 
+                                        "lua_ls",
+                                        "clangd",
+                                        "vimls"
+                                },
+                        })
+
+                end
+        }
+
 }
